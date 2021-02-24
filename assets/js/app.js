04a08969479c872e72ee1d8397b73de43bf926df
hridoypartho <hridoyhalderpartho@gmail.com>
2021-02-24 14:48:22 +0600

    compleate project

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+const calculateTemp = () => {
+    const numberTemp = document.getElementById('temp').value;
+    const tempSelected = document.getElementById('temp_diff');
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
+
+
+    const celToFah = (cel) => {
+        let fahrenheit = Math.round((cel * 9/5) + 32);
+        return fahrenheit;
+     };
+
+    const fahToCel = (fah) => {
+        let calsius = Math.round((fah - 32) * 5/9);
+        return calsius;
+     };
+
+
+
+    let result;
+
+    if(valueTemp == 'cel'){
+        result = celToFah(numberTemp);
+        document.getElementById('resultcontainer').innerHTML = `= ${result} °Fahrenheit`
+    }else{
+        result = fahToCel(numberTemp);
+        document.getElementById('resultcontainer').innerHTML = `= ${result} °Calsius`
+    }
+
+}
